e2c959af97701ff97f6afb4308fc6f325dd6f942
Xin Huang <dev@ret0.dev>
2024-11-20 17:27:05 -0800

    fixed bugs.

--- src/bug2.py
+++ src/bug2.py
@@ -1,4 +1,4 @@
 def connect_to_database():
     username = "admin"  # Sensitive information hardcoded
-    password = "password123"  # Sensitive information hardcoded
+    #password = "password123"  # Sensitive information hardcoded
     print(f"Connecting to database with username {username}")
